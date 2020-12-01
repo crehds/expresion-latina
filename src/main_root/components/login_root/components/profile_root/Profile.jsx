2d777c7173e0422148072f853fb6c8fbcf6ea72b
Unknown <carlos15erh@gmail.com>
2020-12-01 00:13:19 -0500

    Adding and save user image profile

--- src/main_root/components/login_root/components/profile_root/Profile.jsx
+++ src/main_root/components/login_root/components/profile_root/Profile.jsx
@@ -42,6 +42,28 @@ export default class Profile extends Component {
     console.log(result);
   };
 
+  handleSaveUserProfileImage = async (event) => {
+    event.preventDefault();
+    console.log("aqui4");
+    let form = document.getElementById("test__userimageprofile");
+    const formData = new FormData(form);
+    console.log(formData);
+    const response = await fetch("/login/setPathUserProfileImage/1", {
+      method: "PUT",
+      body: formData,
+    }).then((result) => result.json());
+    console.log(response);
+    this.setState({
+      profile: {
+        ...this.state.profile,
+        old_imageProfile: this.state.profile.ruta_imageProfile,
+      },
+    });
+    Swal.fire({
+      title: "Foto Actualizada",
+    });
+  };
+
   componentDidMount() {
     this.props.getFunction(this.updateProfile);
   }
@@ -67,10 +89,73 @@ export default class Profile extends Component {
     });
   };
 
+  handleCancelInput = (event) => {
+    console.log(this.state.profile);
+    let inputCancel = document.getElementById("test__userimageprofile-input");
+    inputCancel.value = "";
+    this.handleInputCheck("none");
+    return this.setState({
+      profile: {
+        ...this.state.profile,
+        ruta_imageProfile: this.state.profile.old_imageProfile,
+        old_imageProfile: "",
+      },
+    });
+  };
+
+  handleInput = (event) => {
+    event.preventDefault();
+    console.log(this.state.profile);
+    let file = event.target.files[0];
+    if (file) {
+      this.handleInputCheck("block");
+      let reader = new FileReader();
+      let image = new Image();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        image.src = reader.result;
+        this.setState((state) => {
+          return state.profile.old_imageProfile
+            ? {
+                profile: {
+                  ...state.profile,
+                  ruta_imageProfile: image.src,
+                },
+              }
+            : {
+                profile: {
+                  ...state.profile,
+                  old_imageProfile: state.profile.ruta_imageProfile,
+                  ruta_imageProfile: image.src,
+                },
+              };
+        });
+      };
+    }
+  };
+
+  handleCheck = () => {
+    let button = document.getElementById("button-profile-head-aux");
+    this.handleInputCheck("none");
+    return button.click();
+  };
+
+  handleInputCheck = (display) => {
+    let inputCheck = document.getElementsByName("check");
+    inputCheck[0].style.display = display;
+    inputCheck[1].style.display = display;
+  };
+
   render() {
     return (
       <div className="profile">
-        <ProfileHead profile={this.state.profile} />
+        <ProfileHead
+          profile={this.state.profile}
+          handleSaveUserProfileImage={this.handleSaveUserProfileImage}
+          handleInput={this.handleInput}
+          handleCancelInput={this.handleCancelInput}
+          handleCheck={this.handleCheck}
+        />
         <ProfileBody
           profile={this.state.profile}
           handleChange={this.handleChange}
--- src/main_root/components/login_root/components/profile_root/components/ProfileHead.jsx
+++ src/main_root/components/login_root/components/profile_root/components/ProfileHead.jsx
@@ -1,16 +1,60 @@
 import React from "react";
 import "../css/profilehead.css";
 export default function ProfileHead(props) {
-  const {profile:user} = props;
+  const { profile: user } = props;
   return (
     <div className="profile-head">
-      <h4 className="profile-head__name">{`${user.name || user.nombre} ${user.lastname || user.apellido}`}</h4>
+      <h4 className="profile-head__name">{`${user.name || user.nombre} ${
+        user.lastname || user.apellido
+      }`}</h4>
       <div className="profile-head__container">
-        <img
-          src={process.env.PUBLIC_URL + "/profile/default-user.png"}
-          alt="foto del admin"
-        />
+        {user.ruta_imageProfile ? (
+          <img src={user.ruta_imageProfile} alt="foto del admin" />
+        ) : (
+          <img
+            src={process.env.PUBLIC_URL + "/profile/default-user.png"}
+            alt="foto del admin"
+          />
+        )}
+        <form
+          id="test__userimageprofile"
+          onSubmit={props.handleSaveUserProfileImage}
+        >
+          <input
+            id="test__userimageprofile-input"
+            type="file"
+            name="imageUserProfile"
+            onChange={props.handleInput}
+          />
+          <button
+            type="submit"
+            form="test__userimageprofile"
+            id="button-profile-head-aux"
+          ></button>
+        </form>
+
+        <i
+          name="check"
+          className="icon3-x edit-userimageprofile3"
+          onClick={props.handleCancelInput}
+        ></i>
+        <i
+          name="check"
+          className="icon3-checkmark edit-userimageprofile2"
+          onClick={props.handleCheck}
+        ></i>
+
+        <i
+          className="icon3-camera edit-userimageprofile"
+          onClick={handleicon}
+        ></i>
       </div>
     </div>
   );
 }
+
+const handleicon = (event) => {
+  let input = document.getElementById("test__userimageprofile-input");
+
+  return input.click();
+};
