a9aefb0df3674a0e4ad0c503498c5ffab3fcea4b
Unknown <carlos15erh@gmail.com>
2020-12-03 18:35:53 -0500

    Adding handlers for edit and save on config

--- src/admin_root/components/ConfigLogin.jsx
+++ src/admin_root/components/ConfigLogin.jsx
@@ -2,8 +2,41 @@ import React, { Component } from "react";
 import "../css/configlogin.css";
 
 export default class ConfigLogin extends Component {
+  state = {
+    inputsValue: [],
+    saveButton: false,
+  };
+  editInputstag = () => {
+    if(!this.state.saveButton) {
+      const inputs = [
+        ...document.querySelectorAll("input[type=text],input[type=date]"),
+      ];
+      const inputsValue = inputs.map((input) => input.value);
+      console.log(inputsValue);
+      this.setState({
+        inputsValue,
+        saveButton:true
+      });
+    } else {
+      const inputs = [
+        ...document.querySelectorAll("input[type=text],input[type=date]"),
+      ];
+      for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = this.state.inputsValue[i];
+      }
+      this.setState({
+        inputsValue:[],
+        saveButton:false
+      });
+      
+    }
+    this.getInputstag();
+  };
+
   getInputstag = () => {
-    const inputs = [...document.querySelectorAll("input[type=text],input[type=date]")];
+    const inputs = [
+      ...document.querySelectorAll("input[type=text],input[type=date]"),
+    ];
     const divsIcons = [
       ...document.getElementsByClassName("profile-icon-input__container"),
     ];
@@ -15,12 +48,29 @@ export default class ConfigLogin extends Component {
     }
   };
 
+  handleSaveInputs = (event) => {
+    event.preventDefault();
+    this.getInputstag();
+    this.props.func();
+    return this.setState({
+      inputsValue: [],
+      saveButton:false
+    })
+  };
+
+  componentWillUnmount(){
+    this.setState({
+      inputValues:[],
+      saveButton: false
+    })
+  }
+
   render() {
     return (
       <div className="config-modal__login">
         <h3>Cuenta</h3>
-        <button onClick={this.getInputstag}>Editar</button>
-        <button onClick={this.props.func}>Guardar</button>
+        <button onClick={this.editInputstag}>Editar</button>
+        <button onClick={this.handleSaveInputs}>Guardar</button>
         <button onClick={this.props.unLogged}>Salir</button>
       </div>
     );
