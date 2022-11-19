3e56f0ba906e1d077f3da597b767497f0afa754d
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-19 05:30:33 -0500

    Apply eslint rules to remaining files

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
+import { Component } from 'react';
 import Header from './header_root/header';
 import './App.css';
 import Main from './main_root/Main';
 import Footer from './footer_root/footer';
 
 export default class App extends Component {
-  state = {
-    content: 'Login',
-    headerFunc: undefined
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: 'Login',
+      headerFunc: undefined,
+    };
+  }
 
   handleContent = (state) => {
     this.setState({
-      content: state
+      content: state,
     });
   };
 
   handleHeadProfile = (func) => this.setState({ headerFunc: func });
 
   render() {
-    const { content } = this.state;
+    const { content, headerFunc } = this.state;
     return (
-      <div className='App'>
+      <div className="App">
         <Header
           handleContent={this.handleContent}
           handleHeadProfile={this.handleHeadProfile}
         />
-        <Main content={content} headerFunc={this.state.headerFunc} />
+        <Main content={content} headerFunc={headerFunc} />
         <Footer />
       </div>
     );
