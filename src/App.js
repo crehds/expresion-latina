ef2c07c408fa3deced5fea2a72783f3de89ae34e
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-21 19:57:11 -0500

    Update index entry

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import { Component } from 'react';
-import Header from './header_root/header';
 import './App.css';
-import Main from './main_root/Main';
-import Footer from './footer_root/footer';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: 'Login',
-      headerFunc: undefined,
-    };
-  }
-
-  handleContent = (state) => {
-    this.setState({
-      content: state,
-    });
-  };
-
-  handleHeadProfile = (func) => this.setState({ headerFunc: func });
-
-  render() {
-    const { content, headerFunc } = this.state;
-    return (
-      <div className="App">
-        <Header
-          handleContent={this.handleContent}
-          handleHeadProfile={this.handleHeadProfile}
-        />
-        <Main content={content} headerFunc={headerFunc} />
-        <Footer />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      Restarting app
+    </div>
+  );
 }
