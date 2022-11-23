220e3adbfb9d244cc7af90270fa1b9d80b46d63b
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-22 22:08:32 -0500

    Refactor Options

--- src/components/Header/components/Menu/components/Options.jsx
+++ src/components/Header/components/Menu/components/Options.jsx
@@ -1,44 +1,50 @@
+import { PureComponent } from 'react';
 import '../css/options.css';
 
-export default function Options() {
-  return (
-    <ul className="options">
-      <li>
-        <div id="Inicio">
-          Inicio
-        </div>
-      </li>
-      <li>
-        <div id="Profesores">
-          Profesores
-        </div>
-      </li>
-      <li>
-        <div id="Clases">
-          Clases
-        </div>
-      </li>
-      <li>
-        <div id="Horario">
-          Horario
-        </div>
-      </li>
-      <li>
-        <div id="Reseñas">
-          Reseñas
-        </div>
-      </li>
-      <li>
-        <div id="Contacto">
-          Encúentranos
-        </div>
-      </li>
-      <li>
-        <div
-          id="Login"
-          aria-label="profile"
-        />
-      </li>
-    </ul>
-  );
+const OPTIONS = [{
+  id: 1,
+  name: 'Inicio',
+},
+{
+  id: 2,
+  name: 'Profesores',
+},
+{
+  id: 3,
+  name: 'Clases',
+},
+{
+  id: 4,
+  name: 'Horario',
+},
+{
+  id: 5,
+  name: 'Reseñas',
+},
+{
+  id: 6,
+  name: 'Encuéntranos',
+},
+];
+
+export default class Options extends PureComponent {
+  componentDidMount() {
+    const options = document.querySelector('.options');
+    const firstOption = options.firstElementChild;
+    firstOption.classList.add('options__li--active');
+  }
+
+  render() {
+    return (
+      <ul className="options">
+        {
+          OPTIONS.map((option) => (
+            <li key={option.id} className="options__li">
+              <p>{option.name}</p>
+            </li>
+          ))
+        }
+      </ul>
+    );
+  }
 }
