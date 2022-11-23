da3ecee7308de8e136fbcc310827754946a3f98c
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-22 21:06:09 -0500

    Refactor Hamburger and Menu

--- src/components/Header/components/Menu/Menu.jsx
+++ src/components/Header/components/Menu/Menu.jsx
@@ -1,43 +1,23 @@
-import { PureComponent } from 'react';
 import './css/menu.css';
 
 import { Hamburger, Options } from './components';
 
-class Menu extends PureComponent {
-  handleIsMenuActive = () => {
-    const hamburger = document.getElementById('hamburger');
-    const hamburgerClass = hamburger.classList;
-    const menuClass = document.getElementById('menu').classList;
+function handleMenu(event) {
+  const hamburger = event.target;
+  const hamburgerStyles = hamburger.classList;
+  const menuStyles = document.querySelector('.menu').classList;
 
-    // TODO: Optimized
-    if (hamburgerClass.contains('is-active')) {
-      hamburgerClass.remove('is-active');
-      menuClass.remove('is-active');
-      hamburger.style.animationName = 'none';
-      document.removeEventListener('click', this.removeListener);
-    } else {
-      hamburgerClass.add('is-active');
-      hamburger.style.animationName = 'gradientefect';
-      menuClass.add('is-active');
-      document.addEventListener('click', this.removeListener);
-    }
-  };
+  hamburgerStyles.toggle('is-active');
+  menuStyles.toggle('is-active');
+}
 
-  removeListener = (event) => {
-    const template = document.getElementById('menu').contains(event.target);
-    if (!template) {
-      this.handleIsMenuActive();
-    }
-  };
-
-  render() {
-    return (
-      <div id="menu" className="menu">
-        <Hamburger handleIsMenuActive={this.handleIsMenuActive} />
-        <Options />
-      </div>
-    );
-  }
+function Menu() {
+  return (
+    <div className="menu">
+      <Hamburger handleMenu={handleMenu} />
+      <Options />
+    </div>
+  );
 }
 
 export default Menu;
--- src/components/Header/components/Menu/components/Hamburger.jsx
+++ src/components/Header/components/Menu/components/Hamburger.jsx
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types';
 import '../css/hamburger.css';
 
 export default function Hamburger(props) {
-  const { handleIsMenuActive } = props;
+  const { handleMenu } = props;
   return (
     <i
       id="hamburger"
       className="icon-menu hamburger-menu"
-      onClick={handleIsMenuActive}
-      onKeyDown={handleIsMenuActive}
+      onClick={handleMenu}
+      onKeyDown={handleMenu}
       role="button"
       tabIndex={0}
       aria-label="hamburger-icon"
@@ -17,5 +17,5 @@ export default function Hamburger(props) {
 }
 
 Hamburger.propTypes = {
-  handleIsMenuActive: PropTypes.func.isRequired,
+  handleMenu: PropTypes.func.isRequired,
 };
