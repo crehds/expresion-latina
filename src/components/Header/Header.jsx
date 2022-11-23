735bb808cb44e81c05b9b877012a45eb289741c6
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-22 21:37:41 -0500

    Convert Header to a functional component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,14 @@
-import { PureComponent } from 'react';
 import { Logo, Menu } from './components';
 
 import './css/header.css';
 
-export default class Header extends PureComponent {
-  render() {
-    return (
-      <header className="header">
-        <Logo />
-        <Menu />
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header className="header">
+      <Logo />
+      <Menu />
+    </header>
+  );
 }
+
+export default Header;
