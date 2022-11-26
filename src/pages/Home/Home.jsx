2c921e56da4d34a74251c6f11b3ec880094e66f1
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-25 19:48:22 -0500

    Apply Poster and fix Carousel styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,10 @@ import Carousel from 'nuka-carousel';
 import { Component } from 'react';
 
 import createAdaptedPosters from './adapters/posters';
+import Poster from './components/Poster';
 import getPosters from './services/posters';
+
+import './css/home.css';
 
 export default class Home extends Component {
   constructor(props) {
@@ -32,58 +35,59 @@ export default class Home extends Component {
     } = this.state;
     let timer;
     return (
-      <div className="home">
-        <Carousel
-          autoplay
-          slideIndex={slideIndex}
-          beforeSlide={() => {
-            clearTimeout(timer);
-          }}
-          afterSlide={(sIndex) => {
-            if (sIndex === posters.length - 1) {
-              timer = setTimeout(() => this.setState({ slideIndex: 0 }), 5000);
-            } else {
-              this.setState({ slideIndex: sIndex });
-            }
-          }}
-          defaultControlsConfig={{
-            pagingDotsStyle: {
-              fill: 'rgba(95, 209, 249, 1)',
-              padding: '0px 20px',
-            },
-          }}
-          renderCenterLeftControls={({ previousSlide }) => (
-            <div className="inicio-arrow">
-              <i
-                className="icon-keyboard_arrow_left"
-                onClick={previousSlide}
-                onKeyDown={previousSlide}
-                role="button"
-                tabIndex={0}
-                aria-label="arrow-left-icon"
-              />
-            </div>
-          )}
-          renderCenterRightControls={({ nextSlide }) => (
-            <div className="inicio-arrow">
-              <i
-                className="icon-keyboard_arrow_right"
-                onClick={nextSlide}
-                onKeyDown={nextSlide}
-                role="button"
-                tabIndex={0}
-                aria-label="arrow-right-icon"
-              />
-            </div>
-          )}
-        >
-          {posters.map((poster) => (
-            <div key={poster.id}>
-              <img src={poster.url} alt={poster.filename} />
-            </div>
-          ))}
-        </Carousel>
-      </div>
+      <Carousel
+        autoplay
+        style={{ height: '100%' }}
+        slideIndex={slideIndex}
+        beforeSlide={() => {
+          clearTimeout(timer);
+        }}
+        afterSlide={(sIndex) => {
+          if (sIndex === posters.length - 1) {
+            timer = setTimeout(() => this.setState({ slideIndex: 0 }), 5000);
+          } else {
+            this.setState({ slideIndex: sIndex });
+          }
+        }}
+        defaultControlsConfig={{
+          pagingDotsStyle: {
+            fill: 'rgba(95, 209, 249, 1)',
+            padding: '0px 20px',
+          },
+        }}
+        renderCenterLeftControls={({ previousSlide }) => (
+          <div className="posters_arrow">
+            <i
+              className="icon-keyboard_arrow_left"
+              onClick={previousSlide}
+              onKeyDown={previousSlide}
+              role="button"
+              tabIndex={0}
+              aria-label="arrow-left-icon"
+            />
+          </div>
+        )}
+        renderCenterRightControls={({ nextSlide }) => (
+          <div className="posters_arrow">
+            <i
+              className="icon-keyboard_arrow_right"
+              onClick={nextSlide}
+              onKeyDown={nextSlide}
+              role="button"
+              tabIndex={0}
+              aria-label="arrow-right-icon"
+            />
+          </div>
+        )}
+      >
+        {posters.map((poster) => (
+          <Poster
+            key={poster.id}
+            filename={poster.filename}
+            url={poster.url}
+          />
+        ))}
+      </Carousel>
     );
   }
 }
