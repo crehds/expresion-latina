354e4ad7fcc14a8ab28a6847a3fdf32ab9ec6a51
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-26 03:54:47 -0500

    Add temporal teachers data

---
+++ src/pages/Teachers/api/teachers.js
@@ -0,0 +1,77 @@
+import izquierdoImage from '../../../assets/images/teachers/bachata_izquierdo.jpg';
+import joanImage from '../../../assets/images/teachers/mambo_joan.jpg';
+import martinezImage from '../../../assets/images/teachers/ballet_martinez.jpg';
+import kikeImage from '../../../assets/images/teachers/body_kike.jpg';
+import pedroImage from '../../../assets/images/teachers/hells_pedro.jpg';
+import stephanyImage from '../../../assets/images/teachers/jazz_stephany.jpg';
+import maryImage from '../../../assets/images/teachers/ladies_mary.jpg';
+import estrellaImage from '../../../assets/images/teachers/latin_estrella.jpg';
+import mishelImage from '../../../assets/images/teachers/mishel_fernandez.jpg';
+
+const TEACHERS = [
+  {
+    id: '1',
+    danceGenre: 'bachata',
+    name: 'Izquierdo',
+    src: izquierdoImage,
+    description: 'Description',
+  },
+  {
+    id: '2',
+    danceGenre: 'mambo',
+    name: 'Joan',
+    src: joanImage,
+    description: 'Description',
+  },
+  {
+    id: '3',
+    danceGenre: 'ballet',
+    name: 'Martinez',
+    src: martinezImage,
+    description: 'Description',
+  },
+  {
+    id: '4',
+    danceGenre: 'body',
+    name: 'Kike',
+    src: kikeImage,
+    description: 'Description',
+  },
+  {
+    id: '5',
+    danceGenre: 'hells',
+    name: 'Pedro',
+    src: pedroImage,
+    description: 'Description',
+  },
+  {
+    id: '6',
+    danceGenre: 'jazz',
+    name: 'Stefanie',
+    src: stephanyImage,
+    description: 'Description',
+  },
+  {
+    id: '7',
+    danceGenre: 'ladies',
+    name: 'Mary',
+    src: maryImage,
+    description: 'Description',
+  },
+  {
+    id: '8',
+    danceGenre: 'latin',
+    name: 'Estrella',
+    src: estrellaImage,
+    description: 'Description',
+  },
+  {
+    id: '9',
+    danceGenre: 'salsa',
+    name: 'Mishel',
+    src: mishelImage,
+    description: 'Description',
+  },
+];
+
+export default TEACHERS;
