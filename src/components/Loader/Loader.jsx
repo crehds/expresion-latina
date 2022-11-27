27a99c0c551c1043b3dc296eb1705e1d4b10b11a
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-27 03:07:34 -0500

    Add Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import { Grid } from './components';
+import './css/loader.css';
+
+function Loader() {
+  return (
+    <div className="loader">
+      <Grid />
+    </div>
+  );
+}
+
+export default Loader;
