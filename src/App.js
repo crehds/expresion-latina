69cf1eeaf18764768778686dc530cee86f1c8b92
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-27 03:08:19 -0500

    Apply dynamic import of components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 
 import { Footer, Header } from './components';
-import Home from './pages/Home/Home';
-import Teachers from './pages/Teachers/Teachers';
+import Loader from './components/Loader/Loader';
 
 import './styles/reset.css';
 import './styles/colors.css';
 import './styles/typography.css';
+import './App.css';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Teachers = lazy(() => import('./pages/Teachers/Teachers'));
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/teachers" element={<Teachers />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/teachers" element={<Teachers />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
