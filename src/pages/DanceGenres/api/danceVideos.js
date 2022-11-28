d8b59c274dbc71b45ca1d4852da3118d479fd000
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-28 17:17:20 -0500

    Add videos const

---
+++ src/pages/DanceGenres/api/danceVideos.js
@@ -0,0 +1,29 @@
+import video1 from '../../../assets/videos/como_llegar.mp4';
+import video2 from '../../../assets/videos/ladies_latinas.mp4';
+import video3 from '../../../assets/videos/video_corto.mp4';
+import video4 from '../../../assets/videos/video_largo.mp4';
+
+const VIDEOS = [
+  {
+    id: 1,
+    description: 'video-random',
+    src: video1,
+  },
+  {
+    id: 2,
+    description: 'video-random',
+    src: video2,
+  },
+  {
+    id: 3,
+    description: 'video-random',
+    src: video3,
+  },
+  {
+    id: 4,
+    description: 'video-random',
+    src: video4,
+  },
+];
+
+export default VIDEOS;
