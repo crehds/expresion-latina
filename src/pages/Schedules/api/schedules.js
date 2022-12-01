f63e807ef46a279c90ca6dd60ad705f8e5718e8c
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-30 22:14:35 -0500

    fix: function to generate random fake schedule
    The past algorithm generate duplicates  day - hour elements.

--- src/pages/Schedules/api/schedules.js
+++ src/pages/Schedules/api/schedules.js
@@ -1,23 +1,29 @@
 import DAYS from './days';
 import HOURS from './hours';
 import DANCE_GENRES from '../../DanceGenres/api/danceGenres';
+import generateRandomNumber from '../../../utils/randomNumber';
 
 const danceGenresLength = DANCE_GENRES.length;
+const daysLength = DAYS.length;
+const hoursLength = HOURS.length;
+const scheduleLength = daysLength * hoursLength;
 
-const SCHEDULES = DAYS.map((day) => {
-  const hours = HOURS.slice().map((hour) => {
-    const random = Math.floor(Math.random() * danceGenresLength);
-    const danceGenre = DANCE_GENRES[random];
-    return {
-      ...hour,
-      danceGenre,
-    };
-  });
+const schedule = [];
 
-  return {
-    ...day,
-    hours,
+while (schedule.length < scheduleLength) {
+  const randomD = generateRandomNumber(daysLength);
+  const randomH = generateRandomNumber(hoursLength);
+  const randomDc = generateRandomNumber(danceGenresLength);
+
+  const newSchedule = {
+    hour: HOURS[randomH].hour,
+    day: DAYS[randomD].name,
+    danceGenre: DANCE_GENRES[randomDc],
   };
-});
 
-export default SCHEDULES;
+  const index = schedule.find((cv) => cv?.hour === newSchedule.hour && cv?.day === newSchedule.day);
+
+  if (!index) schedule.push({ id: schedule.length + 1, ...newSchedule });
+}
+
+export default schedule;
