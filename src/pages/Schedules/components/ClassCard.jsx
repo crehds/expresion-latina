4bc1d79f316b0e7b43fcbc233ed1ba859848f0cb
Carlos Rodriguez <carlos15erh@gmail.com>
2022-11-30 22:26:36 -0500

    Add: class card component with its styles

---
+++ src/pages/Schedules/components/ClassCard.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+import '../css/class-card.css';
+
+function ClassCard(props) {
+  const { danceGenre } = props;
+  return (
+    <div className="class-card">
+      <p>{danceGenre?.name}</p>
+    </div>
+  );
+}
+
+ClassCard.propTypes = {
+  danceGenre: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
+export default ClassCard;
