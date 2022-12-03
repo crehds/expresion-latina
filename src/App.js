d0fc53ed8ab3892b648136ba7aa364ab33ebcdb4
Carlos Rodriguez <carlos15erh@gmail.com>
2022-12-03 03:16:25 -0500

    feat: users reviews page
    Only one fake review at the moment.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const Home = lazy(() => import('./pages/Home/Home'));
 const Teachers = lazy(() => import('./pages/Teachers/Teachers'));
 const DanceGenres = lazy(() => import('./pages/DanceGenres/DanceGenres'));
 const Schedules = lazy(() => import('./pages/Schedules/Schedules'));
+const Reviews = lazy(() => import('./pages/Reviews/Reviews'));
 
 export default function App() {
   return (
@@ -25,6 +26,7 @@ export default function App() {
           <Route path="/teachers" element={<Teachers />} />
           <Route path="/dances/*" element={<DanceGenres />} />
           <Route path="/schedules" element={<Schedules />} />
+          <Route path="/reviews" element={<Reviews />} />
         </Routes>
       </Suspense>
       <Footer />
