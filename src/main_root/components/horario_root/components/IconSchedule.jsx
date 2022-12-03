8cd7d9faf2c293927951ca0f2b3b3af2615b5def
Carlos Rodriguez <carlos15erh@gmail.com>
2022-12-02 23:02:44 -0500

    chore: Remove useless directory
    Directory of the old schedules component

--- src/main_root/components/horario_root/components/IconSchedule.jsx
+++
@@ -1,9 +0,0 @@
-import '../css/iconschedule.css';
-
-export default function IconSchedule() {
-  return (
-    <div className="i-schedule">
-      <i className="icon2-calendar" />
-    </div>
-  );
-}
