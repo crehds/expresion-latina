6b37023b9ba76094b4436fdb12695a690e1ea9d4
Carlos Rodriguez <carlos15erh@gmail.com>
2022-12-05 22:51:00 -0500

    Remove useless directory
    In the past I wanted to create a small config window for admin. It was a terrible idea.

--- src/admin_root/components/AdminContainer.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default function AdminContainer({ children }) {
-  return <div className="admin-modal_container">{children}</div>;
-}
-
-AdminContainer.propTypes = {
-  children: PropTypes.element.isRequired,
-};
