18c0e1fc32764210b4fee723b66fb1fc120d7f15
Carlos Rodriguez <carlos15erh@gmail.com>
2022-12-05 22:47:56 -0500

    style: change layout structure and background color added

--- src/pages/Contact/components/Info.jsx
+++ src/pages/Contact/components/Info.jsx
@@ -3,29 +3,32 @@ import '../css/info.css';
 function Info() {
   return (
     <div className="info">
-      <ul className="info__menu">
-        <li className="info__detail">
-          <i className="icon3-whatsapp" />
-          <p className="text-sm">984578125</p>
-          <i
-            className="icon-content_copy info__copy"
-          />
-        </li>
-        <li className="info__detail">
-          <i className="icon3-phone2" />
-          <p className="text-sm">578125</p>
-          <i
-            className="icon-content_copy info__copy"
-          />
-        </li>
-        <li className="info__detail">
-          <i className="icon3-gmail" />
-          <p className="text-sm">expresionlatina@gmail.com</p>
-          <i
-            className="icon-content_copy info__copy"
-          />
-        </li>
-      </ul>
+      <div className="info__content">
+        <ul className="info__menu">
+          <li className="info__detail">
+            <i className="icon3-whatsapp" />
+            <p className="text-sm">984578125</p>
+            <i
+              className="icon-content_copy info__copy"
+            />
+          </li>
+          <li className="info__detail">
+            <i className="icon3-phone2" />
+            <p className="text-sm">578125</p>
+            <i
+              className="icon-content_copy info__copy"
+            />
+          </li>
+          <li className="info__detail">
+            <i className="icon3-gmail" />
+            <p className="text-sm">expresionlatina@gmail.com</p>
+            <i
+              className="icon-content_copy info__copy"
+            />
+          </li>
+        </ul>
+      </div>
+
     </div>
   );
 }
