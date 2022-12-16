f945afd77b80044a4ff2fda941d010cc0868e049
Carlos Rodriguez <carlos15erh@gmail.com>
2022-12-16 18:47:13 -0500

    chore: Update direction of academy

--- src/pages/Contact/components/Map.jsx
+++ src/pages/Contact/components/Map.jsx
@@ -8,9 +8,10 @@ function Map() {
           className="map__iframe"
           title="google-map"
           key="google-map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3902.7134444423!2d-77.06639216020983!3d-11.994319238797937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105ce570f81ac35%3A0xc005dcfb1c6c7f78!2sAv.%20Jos%C3%A9%20Santos%20Chocano%20469%2C%20Los%20Olivos%2015301!5e0!3m2!1ses!2spe!4v1590017482307!5m2!1ses!2spe"
-          height="300"
-          style={{ border: 0, borderRadius: 5, maxWidth: '100%' }}
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1951.385443483408!2d-77.0731053239558!3d-11.9903491741745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105cf0324c33b47%3A0x19abafda29543543!2sExpresi%C3%B3n%20Latina!5e0!3m2!1ses!2spe!4v1671234284867!5m2!1ses!2spe"
+          width={600}
+          height={450}
+          style={{ border: 0, borderRadius: 5 }}
           allowFullScreen=""
           aria-hidden="false"
         />
